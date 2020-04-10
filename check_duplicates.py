be0312634e5f43aeb42c3086036adc5943536f26
Sherdil <sniyaz@berkeley.edu>
2020-04-10 15:30:05 -0700

    Add an ignore list for check_duplicates.py.

--- check_duplicates.py
+++ check_duplicates.py
@@ -46,8 +46,20 @@ def get_find_files():
 
     return cleaned_files_out
 
+def check_if_ignored(cur_filename, ignore_prefixes):
+    for cur_prefix in ignore_prefixes:
+        if cur_filename.startswith(cur_prefix):
+            return True
+    return False
+
 
 def check_grouped_find_files():
+    ignore_prefixes = [
+        "/usr/lib/node_modules", 
+        "/usr/local/lib/node_modules", 
+        "/usr/local/lib/python2.7/dist-packages",
+        "/usr/lib/python2.7/dist-packages"]
+
     grouped_files = {}
     find_output_files = get_output_files(find_output_dir)
 
@@ -56,6 +68,8 @@ def check_grouped_find_files():
         # NOTE: Also strip ".txt"
         dir_basename = os.path.basename(cur_output_file)[:-4]
         true_dir_name = dir_basename.replace("_", "/")
+        # Let's add that slash in front as well.
+        true_dir_name = "/" + true_dir_name
         print("Find Dir: " + true_dir_name)
 
         grouped_files[true_dir_name] = set()
@@ -74,10 +88,17 @@ def check_grouped_find_files():
             other_dirs = [i for i in grouped_files.keys() if i != cur_check_dir]
             for cur_other_dir in other_dirs:
                 if cur_dir_member in grouped_files[cur_other_dir]:
+                    full_dup_first = os.path.normpath(os.path.join(cur_check_dir, cur_dir_member))
+                    full_dup_second = os.path.normpath(os.path.join(cur_other_dir, cur_dir_member))
+
+                    # Check if we're ignorning duplicates from certain dirs.
+                    if check_if_ignored(full_dup_first, ignore_prefixes) and check_if_ignored(full_dup_second, ignore_prefixes):
+                        continue
+
                     print("")
                     print("DUP: ")
-                    print(os.path.normpath(os.path.join(cur_check_dir, cur_dir_member)))
-                    print(os.path.normpath(os.path.join(cur_other_dir, cur_dir_member)))
+                    print(full_dup_first)
+                    print(full_dup_second)
 
               
 
