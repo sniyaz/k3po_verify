1af19d28009181aa5e6e20a9f1e0b3d6af6f1f83
Sherdil <sniyaz@berkeley.edu>
2020-04-10 14:18:29 -0700

    Check link by line the right way!

--- check_duplicates.py
+++ check_duplicates.py
@@ -7,28 +7,40 @@ dpkg_output_dir = "data/dpkg_search/"
 find_output_dir = "data/find_outputs/"
 
 def get_output_files(cur_dir):
-	files_out = []
-	for file in os.listdir(cur_dir):
-	    if file.endswith(".txt"):
-	       files_out.append(os.path.join(cur_dir, file))
+    files_out = []
+    for file in os.listdir(cur_dir):
+        if file.endswith(".txt"):
+           files_out.append(os.path.join(cur_dir, file))
 
-	return(files_out)
+    return(files_out)
 
 
 def get_dpkg_files():
-	cleaned_files_out = set()
+    cleaned_files_out = set()
 
-	dpkg_output_files = get_output_files(dpkg_output_dir)
-	for cur_file in dpkg_output_files:
-		with open(cur_file) as fp:
-		   cur_line = fp.readline()
-		   while cur_line:
-		       line_parts = cur_line.split(" ")
-		       true_file_name = line_parts[-1]
-		       cleaned_files_out.add(true_file_name)
-		       print(true_file_name)
+    dpkg_output_files = get_output_files(dpkg_output_dir)
+    for cur_file in dpkg_output_files:
+        with open(cur_file) as fp:
+            for cur_line in fp:
+                line_parts = cur_line.split(" ")
+                true_file_name = line_parts[-1]
+                cleaned_files_out.add(true_file_name)
+                # print(true_file_name)
 
-	return cleaned_files_out
-		      
+    return cleaned_files_out
+
+def get_find_files():
+    cleaned_files_out = set()
+
+    find_output_files = get_output_files(find_output_dir)
+    for cur_file in find_output_files:
+        with open(cur_file) as fp:
+            for cur_line in fp:
+                true_file_name = cur_line
+                cleaned_files_out.add(true_file_name)
+                # print(true_file_name)
+
+    return cleaned_files_out
+              
 
 get_dpkg_files()
