e993b3f87ef00cc985df7e230774d7b5904b62bf
Sherdil <sniyaz@berkeley.edu>
2020-04-10 16:47:55 -0700

    Fix find_script.sh to preserve special filenames.

--- find_script.sh
+++ find_script.sh
@@ -1,13 +1,37 @@
-sudo find /usr/lib/gcc/x86_64-linux-gnu/4.8/include ! -type d > ~/Desktop/find_outputs/A.txt
-sudo find /usr/local/include ! -type d > ~/Desktop/find_outputs/B.txt
-sudo find /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed ! -type d > ~/Desktop/find_outputs/C.txt
-sudo find /usr/include/x86_64-linux-gnu ! -type d > ~/Desktop/find_outputs/D.txt
-sudo find /usr/include ! -type d > ~/Desktop/find_outputs/E.txt
+# Includes.
+cd /usr/lib/gcc/x86_64-linux-gnu/4.8/include
+sudo find . ! -type d > ~/Desktop/find_outputs/usr_lib_gcc_x86_64-linux-gnu_4.8_include.txt
 
-sudo find /usr/local/lib/x86_64-linux-gnu ! -type d > ~/Desktop/find_outputs/F.txt
-sudo find /lib/x86_64-linux-gnu ! -type d > ~/Desktop/find_outputs/G.txt
-sudo find /lib64 ! -type d > ~/Desktop/find_outputs/H.txt
-sudo find /usr/lib/x86_64-linux-gnu ! -type d > ~/Desktop/find_outputs/I.txt
-sudo find /usr/local/lib ! -type d > ~/Desktop/find_outputs/J.txt
-sudo find /lib ! -type d > ~/Desktop/find_outputs/K.txt
-sudo find /usr/lib ! -type d > ~/Desktop/find_outputs/L.txt
+cd /usr/local/include
+sudo find . ! -type d > ~/Desktop/find_outputs/usr_local_include.txt
+
+cd /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed
+sudo find . ! -type d > ~/Desktop/find_outputs/usr_lib_gcc_x86_64-linux-gnu_4.8_include-fixed.txt
+
+cd /usr/include/x86_64-linux-gnu
+sudo find . ! -type d > ~/Desktop/find_outputs/usr_include_x86_64-linux-gnu.txt
+
+cd /usr/include
+sudo find . ! -type d > ~/Desktop/find_outputs/usr_include.txt
+
+
+cd /usr/local/lib/x86_64-linux-gnu
+sudo find . ! -type d > ~/Desktop/find_outputs/usr_local_lib_x86_64-linux-gnu.txt
+
+cd /lib/x86_64-linux-gnu
+sudo find . ! -type d > ~/Desktop/find_outputs/lib_x86_64-linux-gnu.txt
+
+cd /lib64
+sudo find . ! -type d > ~/Desktop/find_outputs/lib64.txt
+
+cd /usr/lib/x86_64-linux-gnu
+sudo find . ! -type d > ~/Desktop/find_outputs/usr_lib_x86_64-linux-gnu.txt
+
+cd /usr/local/lib
+sudo find . ! -type d > ~/Desktop/find_outputs/usr_local_lib.txt
+
+cd /lib 
+sudo find . ! -type d > ~/Desktop/find_outputs/lib.txt
+
+cd /usr/lib
+sudo find . ! -type d > ~/Desktop/find_outputs/usr_lib.txt
