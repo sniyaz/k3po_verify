9a856d3f899ade10124103ec1b49d06fae034428
Sherdil <sniyaz@berkeley.edu>
2020-04-12 18:21:06 -0700

    Add duplicate basename checking, but don't use it.

--- check_duplicates.py
+++ check_duplicates.py
@@ -85,6 +85,56 @@ def check_grouped_find_files():
                     print(full_dup_first)
                     print(full_dup_second)
 
+
+# Checks for duplicates based off of the BASE filename.
+def check_grouped_find_files_BASE():
+    ignore_prefixes = [
+        "/usr/include/clang",
+        "/usr/lib/clang",
+        "/usr/include/x86_64-linux-gnu/python3.5m",
+        "/usr/include/python3.5m",
+        "/usr/include/x86_64-linux-gnu/python3.4m",
+        "/usr/include/python3.4m",
+        "/usr/include/x86_64-linux-gnu/python2.7",
+        "/usr/include/python2.7/",
+        "/usr/lib/node_modules", 
+        "/usr/local/lib/node_modules", 
+        "/usr/local/lib/python2.7/dist-packages",
+        "/usr/lib/python2.7/dist-packages"]
+
+    grouped_files = {}
+    find_output_files = get_output_files(find_output_dir)
+
+    # First, load everything in...
+    for cur_output_file in find_output_files:
+        true_dir_name = get_dir_from_find_output_file(cur_output_file)
+        print("Find Dir: " + true_dir_name)
+
+        with open(cur_output_file) as fp:
+            for cur_line in fp:
+                cur_line = cur_line.rstrip('\n')
+
+                true_file_name = os.path.normpath(os.path.join(true_dir_name, cur_line))
+                base_file_name = os.path.basename(true_file_name)
+
+                # NOTE: Remember, only care about headers or static libs.
+                _, file_extension = os.path.splitext(base_file_name)
+                if file_extension != ".a" and file_extension != ".h" and file_extension != ".hpp":
+                    continue
+
+                if base_file_name not in grouped_files:
+                    grouped_files[base_file_name] = [true_file_name]
+                else:
+                    grouped_files[base_file_name].append(true_file_name)
+                    print("BASE COL: ")
+                    print(grouped_files[base_file_name])
+
+                # print(true_file_name)
+                # print(base_file_name)
+
+            fp.close()
+
               
 if __name__ == '__main__':
     check_grouped_find_files()
+    # check_grouped_find_files_BASE()
